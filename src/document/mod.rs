114aad605c284eb0fabd30973cadc521ad12164b
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-11-15 10:39:23 -0800

    Added document concurrency

--- src/document/mod.rs
+++ src/document/mod.rs
@@ -1,38 +1,40 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Document {
-    pub tree: Node,
-    pub authors: HashMap<Uuid, String>,
-    pub generators: HashMap<Uuid, String>,
+    pub tree: Arc<RwLock<Node>>,
+    pub labels: RwLock<HashMap<Uuid, String>>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Node {
-    pub children: Vec<Node>,
+    pub children: Vec<Arc<RwLock<Node>>>,
     pub contents: Vec<NodeContent>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum NodeContent {
     Written(Authored<Value>),
     Comment(Authored<String>),
     Generated(Generated<Value, Value>),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub struct Authored<T> {
     pub author: Uuid,
     pub modified: DateTime<Utc>,
     pub content: T,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub struct Generated<I, O> {
     pub generator: Uuid,
     pub start: DateTime<Utc>,
