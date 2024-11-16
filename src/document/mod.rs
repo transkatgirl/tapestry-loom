d04f8a9f3a0581c98093574a6e772203cefa71e7
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-11-15 18:51:47 -0800

    Improved document concurrency

--- src/document/mod.rs
+++ src/document/mod.rs
@@ -1,23 +1,50 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
+use std::{collections::HashMap, sync::Arc};
 
 use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_json::Value;
+use tokio::sync::{OwnedRwLockReadGuard, OwnedRwLockWriteGuard, RwLock};
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Document {
-    pub tree: Arc<RwLock<Node>>,
-    pub labels: RwLock<HashMap<Uuid, String>>,
+    pub tree: Wrapper<Node>,
+    pub labels: Wrapper<HashMap<Uuid, String>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Node {
-    pub children: Vec<Arc<RwLock<Node>>>,
+    pub children: Vec<Wrapper<Node>>,
     pub contents: Vec<NodeContent>,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Wrapper<T>(Arc<RwLock<T>>);
+
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
+        Wrapper(Arc::new(RwLock::new(value)))
+    }
+
+    pub async fn read(&self) -> OwnedRwLockReadGuard<T> {
+        self.0.clone().read_owned().await
+    }
+
+    pub async fn write(&self) -> OwnedRwLockWriteGuard<T> {
+        self.0.clone().write_owned().await
+    }
+}
+
+impl<T: Serialize> Serialize for Wrapper<T> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        T::serialize(&*self.0.blocking_read(), serializer)
+    }
+}
+
+impl<'de, T: Deserialize<'de>> Deserialize<'de> for Wrapper<T> {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        Ok(Wrapper::new(T::deserialize(deserializer)?))
+    }
 }
 
 impl Node {
